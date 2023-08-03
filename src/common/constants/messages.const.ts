17e49dec23c3fa4202c721ed0ebe8f3e982bca89
Eduardo Pinto <edfepinto@gmail.com>
2023-08-03 07:32:06 -0300

    feat(constants): add messages for sweetalert2

---
+++ src/common/constants/messages.const.ts
@@ -0,0 +1,12 @@
+const MESSAGES = {
+  success: {
+    title: "Sucesso!",
+    text: "Operação realizada com sucesso.",
+  },
+  error: {
+    title: "Erro!",
+    text: "Ocorreu um erro ao realizar a operação.",
+  },
+};
+
+export default MESSAGES;
