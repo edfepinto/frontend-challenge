c42b80ea18acd5da6873ab39a64d5d2b4387dc79
Eduardo Pinto <edfepinto@gmail.com>
2023-08-03 07:36:39 -0300

    fix(components): input duplicated name

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,8 +1,8 @@
-import { Input, InputProps } from "@chakra-ui/react";
+import { Input as ChakraInput, InputProps } from "@chakra-ui/react";
 
 export default function Input(props: InputProps) {
   return (
-    <Input
+    <ChakraInput
       {...props}
       boxShadow="rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;"
     />
