3af89fae6ba3dba175d11e3b0d8d93b461149e3f
Eduardo Pinto <edfepinto@gmail.com>
2023-08-03 07:26:31 -0300

    feat(components): add generic modal

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { createPortal } from "react-dom";
+import { Box } from "@chakra-ui/react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const modalRoot = document.getElementById("modal-root");
+
+function Modal({ isOpen, onClose, children }: ModalProps) {
+  if (!modalRoot || !isOpen) return null;
+
+  return createPortal(
+    <Box
+      position="fixed"
+      top="0"
+      left="0"
+      width="100%"
+      height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="rgba(0, 0, 0, 0.5)"
+      zIndex="base"
+      onClick={onClose}
+      overflow="auto"
+    >
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </Box>,
+    modalRoot
+  );
+}
+
+export default Modal;
